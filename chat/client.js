2445d9b0fbcbb02d5224be9c17ba6818a4b36fc5
Leonardo Souza de Castro <leonardo030605@gmail.com>
2026-09-15 14:55:33 -0300

    métodos finalizadas, falta serialização

--- chat/client.js
+++ chat/client.js
@@ -1,17 +1,27 @@
 import { Request } from "zeromq";
 
 async function run() {
-  console.log("Connecting to server...");
+  console.log("Cliente conectado no broker...");
   const requester = new Request();
 
   requester.connect("tcp://broker:5559");
 
-  await requester.send("Hello");
+  var reply = await login(requester);
+  console.log(reply);
 
-  const [reply] = await requester.receive();
-  console.log("Received reply:", reply.toString());
+  while (reply[0] != "400") {
+    console.log("Nome de usuario incorreto!");
+    reply = await login(requester);
+  }
+  
+  var canais = await listarCanais(requester);
+  console.log("Canais disponiveis: " + canais[1]);
 
-  requester.close();
+  var teste2 = await cadastrarCanal(requester, "canal12");
+  console.log("Resposta do servidor: " + teste2[1]);
+
+
+  await requester.close();
   process.exit(0);
 }
 
@@ -20,3 +30,33 @@ run();
 process.on("SIGINT", function () {
   process.exit(0);
 });
+
+async function login(requester) {
+  var timestamp = Date.now();
+  var mensagemLogin = timestamp + "|login|teste";
+
+  await requester.send(mensagemLogin);
+
+  const [reply] = await requester.receive();
+
+  return reply.toString().split("|");
+}
+
+async function listarCanais(requester) {
+  var timestamp = Date.now();
+  var mensagemListar = timestamp + "|listar";
+  await requester.send(mensagemListar);
+
+  const [reply] = await requester.receive();
+
+  return reply.toString().split("|");
+}
+
+async function cadastrarCanal(requester, canal) {
+  var timestamp = Date.now();
+  var mensagemCadastrar = timestamp + "|cadastrar|" + canal;
+  await requester.send(mensagemCadastrar);
+  const [reply] = await requester.receive();
+
+  return reply.toString().split("|");
+}
--- chat/servidor.js
+++ chat/servidor.js
@@ -1,15 +1,45 @@
 import { Reply } from "zeromq";
+import fs from "fs/promises";
+
+var usuarios = JSON.parse(await fs.readFile("usuarios.json", "utf-8"));
+
+
+var canais = JSON.parse(await fs.readFile("canais.json", "utf-8"));
 
 async function run() {
   const responder = new Reply();
 
   await responder.connect("tcp://broker:5560");
   console.log("Servidor conectado no broker...");
-
+  var requisicao = "";
   for await (const [request] of responder) {
-    console.log("Received request: [", request.toString(), "]");
 
-    await responder.send("World");
+    requisicao = request.toString();
+    requisicao = requisicao.split("|");
+    
+    if (requisicao[1] == "login") {
+      
+      if (usuarios.usuarios.includes(requisicao[2])) {
+        await responder.send("400|Login realizado com sucesso!");
+      } else {
+        await responder.send("404|Nome de usuario incorreto!");
+      }
+      
+    }
+    
+    else if (requisicao[1] == "listar") {
+      await responder.send("400|Canais disponiveis: " + canais.canais.join(", "));
+    }
+    
+    else if (requisicao[1] == "cadastrar") {
+      if (!canais.canais.includes(requisicao[2])) {
+        canais.canais.push(requisicao[2]);
+        await fs.writeFile("canais.json", JSON.stringify(canais), "utf-8");
+        await responder.send("400|Canal " + requisicao[2] + " cadastrado com sucesso!");
+      } else {
+        await responder.send("404|Canal já existe!");
+      }
+    }
   }
 }
 
