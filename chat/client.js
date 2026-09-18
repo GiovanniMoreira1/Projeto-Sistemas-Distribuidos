f29053efa7ca8c2af4b96edee3402c77b6e456f5
Leonardo Souza de Castro <leonardo030605@gmail.com>
2026-09-18 12:45:26 -0300

    parte 1 finalizada

--- chat/client.js
+++ chat/client.js
@@ -1,27 +1,48 @@
 import { Request } from "zeromq";
 
+import protobuf from "protobufjs";
+
 async function run() {
+  var usuarios = ["usuario1", "usuario2", "usuario3","teste", "usuario4", "usuario5"]
+  const root = await protobuf.load("client.proto");
+
+  const Mensagem = root.lookupType("Mensagem");
+  const Resposta = root.lookupType("Resposta");
+
   console.log("Cliente conectado no broker...");
   const requester = new Request();
 
   requester.connect("tcp://broker:5559");
 
-  var reply = await login(requester);
-  console.log(reply);
+  var indiceUsuario = 0;
+  var reply = await login(requester, Mensagem, Resposta, usuarios[indiceUsuario]);
 
-  while (reply[0] != "400") {
+  while (reply.status != "200") {
     console.log("Nome de usuario incorreto!");
-    reply = await login(requester);
+
+    indiceUsuario++;
+
+    if (indiceUsuario >= usuarios.length) {
+      console.log("Nenhum usuario da lista funcionou.");
+      requester.close();
+      process.exit(1);
+    }
+    
+    reply = await login(requester, Mensagem, Resposta, usuarios[indiceUsuario]);
   }
   
-  var canais = await listarCanais(requester);
-  console.log("Canais disponiveis: " + canais[1]);
+  var canais = await listarCanais(requester, Mensagem, Resposta);
+  console.log("Canais disponiveis: " + canais.resposta);
 
-  var teste2 = await cadastrarCanal(requester, "canal12");
-  console.log("Resposta do servidor: " + teste2[1]);
+  var teste2 = await cadastrarCanal(requester, Mensagem, Resposta, "canal14");
+  if (teste2.status == "200") {
+    console.log(teste2.resposta);
+  }
+  else{
+    console.log("Erro:" + teste2.resposta);
+  }
 
-
-  await requester.close();
+  requester.close();
   process.exit(0);
 }
 
@@ -31,32 +52,59 @@ process.on("SIGINT", function () {
   process.exit(0);
 });
 
-async function login(requester) {
+async function login(requester, Mensagem, Resposta, usuario) {
   var timestamp = Date.now();
-  var mensagemLogin = timestamp + "|login|teste";
 
-  await requester.send(mensagemLogin);
+  const payload = Mensagem.create({ timestamp: timestamp, acao: "login", conteudo: usuario });
 
-  const [reply] = await requester.receive();
+  const bufferBinario = Mensagem.encode(payload).finish();
 
-  return reply.toString().split("|");
+  await requester.send(bufferBinario);
+
+  const reply = await requester.receive();
+
+  return deserializarMensagem(Resposta, reply);
 }
 
-async function listarCanais(requester) {
+async function listarCanais(requester, Mensagem, Resposta) {
   var timestamp = Date.now();
-  var mensagemListar = timestamp + "|listar";
-  await requester.send(mensagemListar);
 
-  const [reply] = await requester.receive();
+  const payload = Mensagem.create({ timestamp: timestamp, acao: "listar" });
 
-  return reply.toString().split("|");
+  const bufferBinario = Mensagem.encode(payload).finish();
+
+  await requester.send(bufferBinario);
+
+  const reply = await requester.receive();
+
+  return deserializarMensagem(Resposta, reply);
 }
 
-async function cadastrarCanal(requester, canal) {
+async function cadastrarCanal(requester, Mensagem, Resposta, canal) {
   var timestamp = Date.now();
-  var mensagemCadastrar = timestamp + "|cadastrar|" + canal;
-  await requester.send(mensagemCadastrar);
-  const [reply] = await requester.receive();
 
-  return reply.toString().split("|");
+  const payload = Mensagem.create({ timestamp: timestamp, acao: "cadastrar", conteudo: canal });
+
+  const bufferBinario = Mensagem.encode(payload).finish();
+
+  await requester.send(bufferBinario);
+
+  const reply = await requester.receive();
+
+  return deserializarMensagem(Resposta, reply);
+}
+
+async function deserializarMensagem(Resposta, request) {
+  const buffer = Array.isArray(request) ? request[0] : request;
+
+  const requisicao = Resposta.decode(buffer); // também adicionei o "const" que faltava aqui
+
+  const objetoOriginal = Resposta.toObject(requisicao, {
+    longs: String,
+    enums: String,
+    bytes: String,
+    defaults: true,
+  });
+
+  return objetoOriginal;
 }
--- chat/servidor.js
+++ chat/servidor.js
@@ -1,12 +1,21 @@
 import { Reply } from "zeromq";
 import fs from "fs/promises";
 
-var usuarios = JSON.parse(await fs.readFile("usuarios.json", "utf-8"));
+import protobuf from "protobufjs";
 
+var usuarios = JSON.parse(await fs.readFile("usuarios.json", "utf-8"));
 
 var canais = JSON.parse(await fs.readFile("canais.json", "utf-8"));
 
+var logins = JSON.parse(await fs.readFile("login.json", "utf-8"));
+
 async function run() {
+
+  const root = await protobuf.load("client.proto");
+
+  const Mensagem = root.lookupType("Mensagem");
+  const Resposta = root.lookupType("Resposta");
+
   const responder = new Reply();
 
   await responder.connect("tcp://broker:5560");
@@ -14,30 +23,46 @@ async function run() {
   var requisicao = "";
   for await (const [request] of responder) {
 
-    requisicao = request.toString();
-    requisicao = requisicao.split("|");
+    requisicao = Mensagem.decode(request);
+
+    const objetoOriginal = Mensagem.toObject(requisicao, {
+    longs: String,
+    enums: String,
+    bytes: String,
+    defaults: true,
+  });
     
-    if (requisicao[1] == "login") {
+    if (objetoOriginal.acao == "login") {
       
-      if (usuarios.usuarios.includes(requisicao[2])) {
-        await responder.send("400|Login realizado com sucesso!");
-      } else {
-        await responder.send("404|Nome de usuario incorreto!");
+      if (usuarios.usuarios.includes(objetoOriginal.conteudo)) {
+        const payload = Resposta.create({ status: "200", resposta: "Login realizado com sucesso!" });
+        logins.push({ usuario: objetoOriginal.conteudo, timestamp: objetoOriginal.timestamp, status: "200" });
+        await fs.writeFile("login.json", JSON.stringify(logins), "utf-8");
+        await responder.send(Resposta.encode(payload).finish());
+      } 
+      else {
+        const payload = Resposta.create({ status: "404", resposta: "Nome de usuario incorreto!" });
+        logins.push({ usuario: objetoOriginal.conteudo, timestamp: objetoOriginal.timestamp, status: "404" });
+        await fs.writeFile("login.json", JSON.stringify(logins), "utf-8");
+        await responder.send(Resposta.encode(payload).finish());
       }
       
     }
     
-    else if (requisicao[1] == "listar") {
-      await responder.send("400|Canais disponiveis: " + canais.canais.join(", "));
+    else if (objetoOriginal.acao == "listar") {
+      const payload = Resposta.create({ status: "200", resposta: "Canais disponiveis: " + canais.canais.join(", ") });
+      await responder.send(Resposta.encode(payload).finish());
     }
     
-    else if (requisicao[1] == "cadastrar") {
-      if (!canais.canais.includes(requisicao[2])) {
-        canais.canais.push(requisicao[2]);
+    else if (objetoOriginal.acao == "cadastrar") {
+      if (!canais.canais.includes(objetoOriginal.conteudo)) {
+        canais.canais.push(objetoOriginal.conteudo);
         await fs.writeFile("canais.json", JSON.stringify(canais), "utf-8");
-        await responder.send("400|Canal " + requisicao[2] + " cadastrado com sucesso!");
+        const payload = Resposta.create({ status: "200", resposta: "Canal " + objetoOriginal.conteudo + " cadastrado com sucesso!" });
+        await responder.send(Resposta.encode(payload).finish());
       } else {
-        await responder.send("404|Canal já existe!");
+        const payload = Resposta.create({ status: "404", resposta: "Canal já existe!" });
+        await responder.send(Resposta.encode(payload).finish());
       }
     }
   }
